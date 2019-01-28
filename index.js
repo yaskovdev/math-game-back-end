f10049703561e34c0598bf584f4e0c7cd1ddc3f3
Sergei Iaskov <sergei.iaskov@omniva.ee>
2019-01-28 13:59:34 +0200

    Open WebSocket connection.

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const http = require('http');
+const WebSocketServer = require('websocket').server;
 
 const server = http.createServer((request, response) => {
-    response.writeHead(200);
-    response.end('Welcome to Math Game!');
+	response.writeHead(200);
+	response.end('Welcome to Math Game!');
+});
+
+const webSocketServer = new WebSocketServer({ httpServer: server });
+
+webSocketServer.on('request', request => {
+	const connection = request.accept(null, request.origin);
+
+	connection.sendUTF(JSON.stringify({
+		user: { id: 'id', name: 'Peppa Pig' },
+		challenge: { question: '9 * 3', answer: '28' }
+	}));
+
+	connection.on('message', message => {
+		if (message.type === 'utf8') {
+			console.log('Message received', message)
+		}
+	})
 });
 
 server.listen(8080);
